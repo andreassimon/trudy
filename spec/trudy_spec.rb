4f252562e5efae82a1a86651db64adf9e9ece9aa
Andreas Simon <a.simon@quagilis.de>
2012-02-16 13:45:01 +0100

    Added some regression tests for the choreography

--- spec/trudy_spec.rb
+++ spec/trudy_spec.rb
@@ -18,25 +18,43 @@ describe Trudy do
     last_response.body.should == "ping localhost:3456\nbroad localhost:3456"
   end
 
-  ["cancelled", "failure", "hanging", "success"].each do |status|
-    it "responds to '#{ status }'" do
-      #exchange.publish(status, :key => 'trudy')
-      post "/", :buildResult => status
-      last_response.status.should == 201
+  CHOREOGRAPHY = {
+      "success"   => "\x00\x00\x00\n\x00\b\x00\x00\x00\x00\b\x01\x00\x00\x00\x00\x00\x00",
+      "cancelled" => "\x00\x00\x00\n\x00\b\x00\a\x00\x00\b\x01\a\x00\x00\x00\x00\x00",
+      "failure"   => "\x00\x00\x00\n\x00\b\x00\n\x00\x00\b\x01\n\x00\x00\x00\x00\x00",
+      "hanging"   => "\x00\x00\x00\n\x00\b\x00\r\x00\x00\b\x01\r\x00\x00\x00\x00\x00"
+  }
 
-      get '/vl/p4.jsp'
-      last_response.should be_ok
+  CHOREOGRAPHY.each do |status, choreo|
+
+    describe "for status '#{status}'" do
+
+      it "responds to '#{ status }'" do
+        #exchange.publish(status, :key => 'trudy')
+        post "/", :buildResult => status
+        last_response.status.should == 201
+
+        get '/vl/p4.jsp'
+        last_response.should be_ok
+      end
+
+      it "sends files" do
+        get "/#{status}.mp3"
+
+        last_response.status.should == 200
+      end
+
+      it "sends choreography" do
+        get "/#{status}.nab"
+
+        last_response.status.should == 200
+        last_response.body.should == choreo
+      end
     end
   end
 
   it "sends bootcode" do
     get "/bc.jsp"
-
-    last_response.status.should == 200
-  end
-
-  it "sends files" do
-    get "/failure.mp3"
 
     last_response.status.should == 200
   end
